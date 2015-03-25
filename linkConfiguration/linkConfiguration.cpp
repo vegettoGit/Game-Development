4d298116dff89286453855aec56655de2d905105
Diego <diego.delpozo@gmail.com>
2015-03-25 23:19:25 +0000

    Finishing up link configuration solver algorithm computations. Next we will be creating a test and debugging it with some real cases

--- linkConfiguration/linkConfiguration.cpp
+++ linkConfiguration/linkConfiguration.cpp
@@ -1,4 +1,5 @@
 #include "linkConfiguration.h"
+#include <math.h>
 
 LinkConfiguration::LinkConfiguration()
 {
@@ -50,8 +51,46 @@ Vector3 LinkConfiguration::Solve(const Vector3& link1StartPoint, const Vector3& 
 
    */
 
-   // TODO
+   // Since the start point in our local coordinates is at (0, 0, 0), c = Length (V1)
+   Vector3 v1 = link2EndPoint - link1StartPoint;
 
+   Vector3 v2;
+   if (v1.m_x == 0 && v1.m_y == 0)
+   {
+      v2.m_x = 1;
+      v2.m_y = 0;
+      v2.m_z = 0;
+   }
+   else
+   {
+      v2.m_x = -v1.m_y;
+      v2.m_y = v1.m_x;
+      v2.m_z = 0;
+   }
+
+   float c = v1.length();
+
+   // Solution on plane XY
+   float l1Square = link1Length * link1Length;
+   float a = (c + (l1Square - link2Length * link2Length / c)) / 2.0f;
+   float b = sqrt(l1Square - a * a);
+
+   // Translate into P coordinates
    Vector3 result;
+   
+   result.m_x = a * v1.m_x;
+   result.m_y = a * v1.m_y;
+   result.m_z = a * v1.m_z;
+   result.m_x += b * v2.m_x;
+   result.m_y += b * v2.m_y;
+   result.m_z += b * v2.m_z;
+
+   // Translate into world coordinates
+   result.m_x += link1StartPoint.m_x;
+   result.m_y += link1StartPoint.m_y;
+   result.m_z += link1StartPoint.m_z;
+
+   // TODO: Testing and debugging
+
    return result;
 }
