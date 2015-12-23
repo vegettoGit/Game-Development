664524134a54bf20bab82e3590c55f0b9bba073b
Diego <diego.delpozo@gmail.com>
2015-12-23 16:01:08 +0000

    - Well defined jobs as part of the job queue class. - Preparing for using a unique_lock and condition variable so that different threads can use a job queue. - We can now push jobs into a given job queue.

--- task/jobQueue.h
+++ task/jobQueue.h
@@ -5,6 +5,10 @@
 
 */
 
+#include <deque>
+#include <mutex>
+#include <functional>
+
 class JobQueue
 {
 
@@ -14,8 +18,22 @@ class JobQueue
    JobQueue               (JobQueue&& jobQueue)      = delete;
    JobQueue&  operator =  (JobQueue&& jobQueue)      = delete;
 
+   std::deque<std::function<void()>> m_jobs;
+   std::mutex                        m_mutex;
+   std::condition_variable           m_ready;
+
 public:
 
    JobQueue();
    ~JobQueue();
+
+   template<typename F>
+   void push(F&& job) 
+   {
+      {
+         std::unique_lock<std::mutex> lock{ m_mutex };
+         m_jobs.emplace_back(std::forward<F>(job));
+      }
+      m_ready.notify_one();
+   }
 };
